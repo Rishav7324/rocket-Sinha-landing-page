ff1b9b429fc2e1f8ee512d839d5bab46b51f6862
RISHAV RAJ <rishavraj74883@gmail.com>
2025-10-07 10:14:58 +0000

    Increase image size (_for element img_)

--- src/app/components/landing/results.tsx
+++ src/app/components/landing/results.tsx
@@ -39,7 +39,7 @@ const ResultsSection = () => {
               {resultImages.map((image) => (
                 <CarouselItem
                   key={image.id}
-                  className="md:basis-1/2 lg:basis-1/4"
+                  className="sm:basis-1/2 lg:basis-1/3"
                 >
                   <div className="p-1">
                     <Card className="overflow-hidden">
